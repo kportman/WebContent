0728ac5c34d3091678dd547d4393810bea0b2082
Shakedkilim <Shakedkilim@gmail.com>
2016-02-20 21:55:30 +0200

    Existing and By topic questions

--- appController.js
+++ appController.js
@@ -111,51 +111,200 @@ app.controller('profileCont', ['$scope', '$http',
                            	
     }]);//end profileCont controller
 
+app.controller('byTopicCont', ['$scope', '$http', 'appFactory',
+                            	function ($scope, $http, appFactory){
+                    $scope.questions = {};
+                    $scope.topics = {};
+                    $scope.prevNextShow = false;
+                    $scope.show = true;
+                    $scope.hide = false;
+                    $scope.offset = 0;
+                    
+                    $scope.bringQByTopic= function(topic){
+                    	$scope.display(topic,$scope.offset);
+                    }
+                    
+                    $scope.bringTopics = function(){
+                    	$http({ 
+         				   method: 'GET',
+         				   url: '/FunItaly/byTopic',
+         				   params: { get: "allTopics" },
+         				   headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+         				}).success(function(data){
+         					$scope.topics = data;
+         					});
+                    }
+                    $scope.voteQuesUp = function(qid){
+                  	  //check if user published this question
+                  	  appFactory.vote($http, qid, 1, "question", 1);
+                    }
+                    $scope.voteQuesDown = function(qid){
+                  	  appFactory.vote($http, qid, 1, "question", -1);
+                    }
+                    
+                    $scope.voteAnsUp = function(qid, aid){
+                  	  appFactory.vote($http, qid, aid, "answer", 1);
+                    }
+                    
+                    $scope.voteAnsDown = function(qid, aid){
+                  	  appFactory.vote($http, qid, aid, "answer", -1);
+                    }
+                    
+                    $scope.prev = function(type){
+                  	  if(($scope.offset - 20) >= 0)
+              		  {
+                  		  //disable prev
+                  		  $scope.offset = $scope.offset - 20;
+                  		  $scope.display(type,$scope.offset);
+              		  }
+                  	  
+                    }
+                    $scope.next = function(type){
+                  	  if(!($scope.questions.length < 20))
+                  	  {
+                  		  //disable next
+                  		  $scope.offset = $scope.offset + 20;
+                      	  $scope.display(type,$scope.offset);
+                  	  } 
+                    }
+                    
+                    $scope.display = function(type, offs){
+                  	  var req = appFactory.display($http,type, offs);
+                  	  req.success(function(data){$scope.questions = data;});
+                    }
+                    
+                    $scope.getAnswers = function(id, index){// gets question id and returns the answers
+                  	  var req = appFactory.getAnswers($http,id);
+                  	  req.success(function(data){$scope.questions[index].answers = data;});
+                  	  //bug is here?(does not always show the answer)
+                    }    
+                      
+                    $scope.publish = function(qid, ans, ansNow, index){
+                  	  appFactory.publish($http,qid, window.localStorage.nickname, ans);
+                  	  $scope.getAnswers(qid, index);  
+                    }
+     }]);//end byTopicCont controller
+
+app.controller('existingCont', ['$scope', '$http', 'appFactory',
+                            	function ($scope, $http, appFactory){
+                    $scope.questions = {};
+                    $scope.show = true;
+                    $scope.hide = false;
+                    $scope.offset = 0;
+                    
+                    $scope.voteQuesUp = function(qid){
+                  	  //check if user published this question
+                  	  appFactory.vote($http, qid, 1, "question", 1);
+                    }
+                    $scope.voteQuesDown = function(qid){
+                  	  appFactory.vote($http, qid, 1, "question", -1);
+                    }
+                    
+                    $scope.voteAnsUp = function(qid, aid){
+                  	  appFactory.vote($http, qid, aid, "answer", 1);
+                    }
+                    
+                    $scope.voteAnsDown = function(qid, aid){
+                  	  appFactory.vote($http, qid, aid, "answer", -1);
+                    }
+                    
+                    $scope.prev = function(type){
+                  	  if(($scope.offset - 20) >= 0)
+              		  {
+                  		  //disable prev
+                  		  $scope.offset = $scope.offset - 20;
+                  		  $scope.display(type,$scope.offset);
+              		  }
+                  	  
+                    }
+                    $scope.next = function(type){
+                  	  if(!($scope.questions.length < 20))
+                  	  {
+                  		  //disable next
+                  		  $scope.offset = $scope.offset + 20;
+                      	  $scope.display(type,$scope.offset);
+                  	  } 
+                    }
+                    
+                    $scope.display = function(type, offs){
+                  	  var req = appFactory.display($http,type, offs);
+                  	  req.success(function(data){$scope.questions = data;});
+                    }
+                    
+                    $scope.getAnswers = function(id, index){// gets question id and returns the answers
+                  	  var req = appFactory.getAnswers($http,id);
+                  	  req.success(function(data){$scope.questions[index].answers = data;});
+                  	  //bug is here?(does not always show the answer)
+                    }    
+                      
+                    $scope.publish = function(qid, ans, ansNow, index){
+                  	  appFactory.publish($http,qid, window.localStorage.nickname, ans);
+                  	  $scope.getAnswers(qid, index);  
+                    }
+     }]);//end existingCont controller
+
 app.controller('newestCont', ['$scope', '$http', 'appFactory',
                               	function ($scope, $http, appFactory){
                       $scope.questions = {};
-                      $scope.answers = {};
+                      $scope.show = true;
+                      $scope.hide = false;
+                      $scope.offset = 0;
                       
-                      $scope.show = false;
-                      $scope.hide = true;
-
-                      $scope.display = function(){
-                    	  alert("I'm in display function");
-	                      $http({ 
-	      					method: 'GET',
-	      					url: '/FunItaly/getQuestions',
-	      					params: { getQuestions: "newest" , offset: 0 },
-	      					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-	      				}).success( function(data) {
-	      					$scope.questions = data;
-	      				});
+                      $scope.voteQuesUp = function(qid){
+                    	  //check if user published this question
+                    	  appFactory.vote($http, qid, 1, "question", 1);
                       }
-                      $scope.getAnswers = function(id){
-	                      $http({ 
-		      					method: 'GET',
-		      					url: '/FunItaly/answer',
-		      					params: { qid: id },
-		      					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		      				}).success( function(data) {
-		      					$scope.answers = data;
-		      				});
+                      $scope.voteQuesDown = function(qid){
+                    	  appFactory.vote($http, qid, 1, "question", -1);
+                      }
+                      
+                      $scope.voteAnsUp = function(qid, aid){
+                    	  appFactory.vote($http, qid, aid, "answer", 1);
+                      }
+                      
+                      $scope.voteAnsDown = function(qid, aid){
+                    	  appFactory.vote($http, qid, aid, "answer", -1);
+                      }
+                      
+                      $scope.prev = function(type){
+                    	  if(($scope.offset - 20) >= 0)
+                		  {
+                    		  //disable prev
+                    		  $scope.offset = $scope.offset - 20;
+                    		  $scope.display(type,$scope.offset);
+                		  }
+                    	  
+                      }
+                      $scope.next = function(type){
+                    	  if(!($scope.questions.length < 20))
+                    	  {
+                    		  //disable next
+                    		  $scope.offset = $scope.offset + 20;
+                        	  $scope.display(type,$scope.offset);
+                    	  } 
+                      }
+                      
+                      $scope.display = function(type, offs){
+                    	  var req = appFactory.display($http,type, offs);
+                    	  req.success(function(data){$scope.questions = data;});
+                      }
+                      
+                      $scope.getAnswers = function(id, index){// gets question id and returns the answers
+                    	  var req = appFactory.getAnswers($http,id);
+                    	  req.success(function(data){$scope.questions[index].answers = data;});
+                    	  //bug is here?(does not always show the answer)
                       }    
                         
-                      
-                   
-                      $scope.publish = function(qid, nick, ans){
-                    	 // $scope.writeReply = false;
-                    	 // $scope.showReply = true;
-                    	  //$scope.questins[index] = "";
-                    	  appFactory.publish($http,qid, nick, ans);
-                    	  
+                      $scope.publish = function(qid, ans, ansNow, index){
+                    	  appFactory.publish($http,qid, window.localStorage.nickname, ans);
+                    	  $scope.getAnswers(qid, index);  
                       }
        }]);//end newestCont controller
 
 app.factory("appFactory", function() {
 
 	return {
-		publish: function($http,qid, nick, ans) {
+		publish: function($http, qid, nick, ans) {
       	  $http({
    			method:'POST',
    			url:"/FunItaly/answer",
@@ -178,7 +327,50 @@ app.factory("appFactory", function() {
 			}
    		});// end success 
 
-		}
+		},//end publish
+		
+		vote: function($http, qid, aid, type, upOrDown) {
+	      	  $http({
+	   			method:'POST',
+	   			url:"/FunItaly/rating",
+	   			transformRequest: function(arr) {
+	   				
+	   				var string = [];
+	   				for( var i in arr )
+	   				{
+	   					string.push(encodeURIComponent(i) + "=" + encodeURIComponent(arr[i]));
+	   				}
+	   				return string.join("&");
+	   			},
+	   			data: { voteType: type, vote: upOrDown, quesID: qid, ansID: aid },
+	   			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	   		}).success( function(info){
+	   			if(info == "Ok")
+				{
+	   				alert("Ok");
+	   				
+				}
+	   		});// end success 
+
+			},//end vote
+		getAnswers: function($http,id){
+
+			return $http({ 
+					method: 'GET',
+					url: '/FunItaly/answer',
+					params: { qid: id },
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				})
+		}, //end getAnswers
+	   display: function($http,type, offs){
+		   
+		  return  $http({ 
+				   method: 'GET',
+				   url: '/FunItaly/getQuestions',
+				   params: { getQuestions: type , offset: offs },
+				   headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				}) 
+	   } //end display
 	};
 });
 
